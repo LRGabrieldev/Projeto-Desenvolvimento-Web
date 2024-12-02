000a7bb773b1070cc920c2b1272fc58e5dfcf861
João Gabriel Loureiro <146102111+LRGabrieldev@users.noreply.github.com>
2024-12-02 16:44:18 -0300

    Add files via upload

--- Projeto 2/Página Principal/jsgestao.js
+++ Projeto 2/Página Principal/jsgestao.js
@@ -11,5 +11,44 @@ function addTab(){
     const vDespesas = Number(despesas);
     const vSaldo = Number(saldo);
 
-    if(data === "" || descricao === "" || categoria === "" || receita === "" || despesas === "")
+    if(data === "" || descricao === "" || categoria === "" || receita === "" || despesas === "" || saldo === "") {
+        exibirMensagem("Preencha todos os campos!");
+    }
+
+    if(isNaN(receita) || isNaN(despesas) || isNaN(saldo)) {
+        exibirMensagem("Nas abas Receita, despesas ou saldo, digite apenas números!");
+    }
+
+    const tb = document.getElementById("tabela").getElementsByTagName("tbody")[0];
+
+    const linha = tb.insertRow();
+
+    const colData = linha.insertCell(0);
+    const colDescricao = linha.insertCell(1);
+    const colCategoria = linha.insertCell(2);
+    const colReceita = linha.insertCell(3);
+    const colDespesas = linha.insertCell(4);
+    const colSaldo = linha.insertCell(5);
+
+    colData.innerHTML = data;
+    colDescricao.innerHTML = descricao;
+    colCategoria.innerHMTL = categoria;
+    colReceita.innerHTML = vReceita;
+    colDespesas.innerHTML = vDespesas;
+    colSaldo.innerHTML = vSaldo;
+
+    document.getElementById("data").value = "";
+    document.getElementById("descricao").value = "";
+    document.getElementById("categoria").value = "";
+    document.getElementById("receita").value = "";
+    document.getElementById("despesas").value = "";
+    document.getElementById("saldo").value = "";
+}
+
+function exibirMensagem(texto) {
+    const mensagemContainer = document.getElementById("mensagem-container");
+    const mensagemLabel = document.getElementById("mensagem");
+
+    mensagemLabel.textContent = texto;
+    mensagemContainer.style.display = "block";
 }
